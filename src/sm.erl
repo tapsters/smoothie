6c7287c2517ce14fdb8ec853684b5ffc72d21a42
Vitaly Shutko <sokal32@gmail.com>
2015-09-01 19:15:47 +0300

    migrate to cowboy2 (WIP), add functions to work with dates, add some specs

--- src/sm.erl
+++ src/sm.erl
@@ -23,12 +23,24 @@
 -export([bin_to_hex/1]).
 
 -export([now/0]).
+-export([parse_http_date/1]).
 
 %% Debug
 -export([stacktrace/0]).
 
+-type unixtime() :: integer().
+-type route() :: 
+  {file, Path :: string()} |
+  {dir, Path :: string()} |
+  {priv_file, App :: atom(), Path :: string()} |
+  {priv_dir, App :: atom(), Path :: string()} |
+  {request, Module :: atom(), Function :: atom()} |
+  {websocket, Handler :: atom(), Protocol :: atom()} |
+  {websocket, Handler :: atom(), Protocol :: atom(), Timeout :: integer()}.
+
 %% Setting up
 
+-spec start_http(Opts :: [term()]) -> {ok, pid()} | {error, any()}.
 start_http(Opts) ->
   TransOpts     = prop(ranch,  Opts, [{port, 3000}]),
   CowboyOpts    = prop(cowboy, Opts, [{nb_acceptors, 100},
@@ -43,6 +55,7 @@ start_http(Opts) ->
 
   cowboy:start_http(http, NbAcceptors, TransOpts, ProtoOpts2).
 
+-spec route({Pattern :: string(), Route :: route()}) -> tuple(). %% cowboy_router:route_rule()
 route({Pattern, {file, Path}})                   -> {Pattern, cowboy_static, {file, Path}};
 route({Pattern, {dir, Path}})                    -> {Pattern, cowboy_static, {dir, Path}};
 route({Pattern, {priv_file, App, Path}})         -> {Pattern, cowboy_static, {priv_file, App, Path}};
@@ -57,6 +70,7 @@ route({Pattern, {websocket, Handler, Protocol, Timeout}}) -> {Pattern, sm_websoc
                                                                {protocol, Protocol},
                                                                {timeout, Timeout}]}.
 
+-spec routes(Routes :: cowboy_router:routes()) -> cowboy_router:dispatch_rules().
 routes(Routes) ->
   Routes1 = [route(Route) || Route <- Routes],
   cowboy_router:compile(
@@ -64,48 +78,70 @@ routes(Routes) ->
 
 %% Formats
 
+-spec json_enc(Data :: tuple()) -> list().
 json_enc(Data) ->
   yaws_json2:encode(Data).
 
+-spec json_dec(Data :: list()) -> tuple().
 json_dec(Data) ->
   {ok, Document} = yaws_json2:decode_string(Data), Document.
 
 %% Utils
 
+-spec env(App :: atom(), Key :: atom()) -> any().
 env(App, Key)          -> env(App, Key, undefined).
 env(App, Key, Default) ->
   application:get_env(App, Key, Default).
 
+-spec env_set(App :: atom(), Key :: atom(), Value :: any()) -> any().
+-spec env_set(App :: atom(), Key :: atom(), Value :: any(), Timeout :: integer()) -> any().
+-spec env_set(App :: atom(), Key :: atom(), Value :: any(), Timeout :: integer(), Persistent :: boolean()) -> any().
 env_set(App, Key, Value)                      -> env_set(App, Key, Value, 5000, false).
 env_set(App, Key, Value, Timeout)             -> env_set(App, Key, Value, Timeout, false).
 env_set(App, Key, Value, Timeout, Persistent) ->
   application:set_env(App, Key, Value, [{timeout, Timeout}, {persistent, Persistent}]).
 
-prop(Key, List) -> prop(Key, List, none).
+-spec prop(Key :: atom(), List :: [term()]) -> any().
+prop(Key, List)          -> prop(Key, List, none).
 prop(Key, List, Default) ->
   case lists:keyfind(Key, 1, List) of
     {_, Value} -> Value;
     _ -> Default
   end.
 
+-spec prop_replace(Key :: atom(), List :: [term()], Value :: any()) -> [term()].
 prop_replace(Key, List, Value) ->
   case lists:keyfind(Key, 1, List) of
     false -> [{Key, Value}|List];
     _ -> lists:keyreplace(Key, 1, List, {Key, Value})
   end.
 
+-spec now() -> unixtime().
 now() ->
-  {Mega, Sec, Micro} = os:timestamp(),
+  timestamp_to_unixtime(os:timestamp()).
+
+-spec parse_http_date(Date :: binary() | string()) -> unixtime().
+parse_http_date(Date) when is_list(Date) -> parse_http_date(list_to_binary(Date));
+parse_http_date(Date)                    ->
+  Seconds = calendar:datetime_to_gregorian_seconds(cow_date:parse_date(Date)) - 62167219200,
+  Seconds * 1000.
+
+-spec timestamp_to_unixtime(erlang:timestamp()) -> unixtime().
+timestamp_to_unixtime({Mega, Sec, Micro}) ->
   (Mega * 1000000 + Sec) * 1000 + round(Micro/1000).
 
 %%(c) Steve Vinoski
-bin_to_hex(Bin) ->
-  lists:flatten([io_lib:format("~2.16.0B", [X]) || X <- binary_to_list(Bin)]).
 
-hex_to_bin(List) when is_binary(List) -> hex_to_bin(binary_to_list(List), []);
-hex_to_bin(List) when is_list(List)   -> hex_to_bin(List, []).
-hex_to_bin([], Acc)                   -> list_to_binary(lists:reverse(Acc));
-hex_to_bin([X,Y|T], Acc)              ->
+-spec bin_to_hex(Value :: binary()) -> binary().
+bin_to_hex(Value) ->
+  lists:flatten([io_lib:format("~2.16.0B", [X]) || X <- binary_to_list(Value)]).
+
+-spec hex_to_bin(Bin :: binary() | string()) -> binary().
+hex_to_bin(Value) when is_binary(Value) -> hex_to_bin(binary_to_list(Value), []);
+hex_to_bin(Value) when is_list(Value)   -> hex_to_bin(Value, []).
+
+hex_to_bin([], Acc)      -> list_to_binary(lists:reverse(Acc));
+hex_to_bin([X,Y|T], Acc) ->
   {ok, [V], []} = io_lib:fread("~16u", [X,Y]),
   hex_to_bin(T, [V | Acc]).
 
--- src/sm_websocket.erl
+++ src/sm_websocket.erl
@@ -1,10 +1,8 @@
 -module(sm_websocket).
 -author("Vitaly Shutko").
--behaviour(cowboy_http_handler).
--behaviour(cowboy_websocket_handler).
+-behaviour(cowboy_websocket).
 
--export([init/3, handle/2, terminate/3]).
--export([websocket_init/3, websocket_handle/3, websocket_info/3, websocket_terminate/3]).
+-export([init/2, websocket_handle/3, websocket_info/3, terminate/3]).
 
 -type handler_state() :: any().
 -type cowboy_req() :: tuple().
@@ -16,8 +14,7 @@
   | {info, any()}
   | terminate.
 
--callback handle(message(), state())
-	-> any().
+-callback handle(message(), state()) -> any().
 
 -record(state, {
   handler,
@@ -27,60 +24,25 @@
 
 %% HTTP
 
-init(_Transport, Req, Opts) ->
-  {handler, Handler} = proplists:lookup(handler, Opts),
+init(Req, Opts) ->
+  {handler,  Handler}  = proplists:lookup(handler, Opts),
+  {protocol, Protocol} = proplists:lookup(protocol, Opts),
+  {timeout,  Timeout}  = proplists:lookup(timeout, Opts),
+  
+  State = #state{handler=Handler, protocol=Protocol, handler_state={}},
 
-  case cowboy_req:header(<<"upgrade">>, Req) of
-    {undefined, Req2} ->
-      {shutdown, Req2, undefined};
-    {Bin, Req2} when is_binary(Bin) ->
-      case cowboy_bstr:to_lower(Bin) of
-        <<"websocket">> ->
-          case Handler:handle(handshake, {undefined, Req}) of
-            ok ->
-              {upgrade, protocol, cowboy_websocket, Req2, Opts++[{handler_state, {}}]};
-            {ok, HandlerState} ->
-              {upgrade, protocol, cowboy_websocket, Req2, Opts++[{handler_state, HandlerState}]};
-            shutdown ->
-              {ok, Req3} = cowboy_req:reply(500, [], [], Req2),
-              {shutdown, Req3, undefined};
-            {shutdown, StatusCode} ->
-              {ok, Req3} = cowboy_req:reply(StatusCode, [], [], Req2),
-              {shutdown, Req3, undefined}
-          end;
-        _ ->
-          {ok, Req3} = cowboy_req:reply(501, [], [], Req2),
-          {shutdown, Req3, undefined}
-      end;
-    {_, Req2} -> {shutdown, Req2, undefined}
+  case Handler:handle(init, {{}, Req}) of
+    ok ->
+      {cowboy_websocket, Req, State, Timeout, hibernate};
+    {ok, HandlerState} ->
+      {cowboy_websocket, Req, State#state{handler_state=HandlerState}, Timeout, hibernate};
+    shutdown ->
+      {ok, cowboy_req:reply(500, Req), State};
+    {shutdown, StatusCode} ->
+      {ok, cowboy_req:reply(StatusCode, Req), State}
   end.
-
-handle(Req, State) ->
-  {ok, Req, State}.
-
-terminate(_Reason, _Req, _State) ->
-  ok.
 
 %% WebSocket
-
-websocket_init(_Transport, Req, Opts) ->
-  {handler,       Handler}      = proplists:lookup(handler, Opts),
-  {timeout,       Timeout}      = proplists:lookup(timeout, Opts),
-  {protocol,      Protocol}     = proplists:lookup(protocol, Opts),
-  {handler_state, HandlerState} = proplists:lookup(handler_state, Opts),
-
-  State = #state{handler=Handler, protocol=Protocol},
-
-  case Handler:handle(init, {HandlerState, Req}) of
-    ok ->
-      {ok, Req, State#state{handler_state=undefined}, Timeout, hibernate};
-    {ok, HandlerState1} ->
-      {ok, Req, State#state{handler_state=HandlerState1}, Timeout, hibernate};
-    shutdown ->
-      {shutdown, Req};
-    {shutdown, _HandlerState} ->
-      {shutdown, Req}
-  end.
 
 websocket_handle({text, <<"ping">>}, Req, State) ->
   {reply, {text, <<"pong">>}, Req, State, hibernate};
@@ -102,9 +64,9 @@ websocket_handle({Format, Data}, Req, State=#state{handler=Handler,
           Encoded = Protocol:encode(Reply, Format),
           {reply, {Format, Encoded}, Req, State#state{handler_state=HandlerState2}, hibernate};
         shutdown ->
-          {shutdown, Req, State};
+          {stop, Req, State};
         {shutdown, HandlerState2} ->
-          {shutdown, Req, State#state{handler_state=HandlerState2}}
+          {stop, Req, State#state{handler_state=HandlerState2}}
       end;
     false ->
       io:format("Protocol ~p doesn't support ~p format~n", [Protocol, Format]),
@@ -126,11 +88,11 @@ websocket_info(Info, Req, State=#state{handler=Handler,
       Encoded = Protocol:encode(Data, Format),
       {reply, {Format, Encoded}, Req, State#state{handler_state=HandlerState2}, hibernate};
     shutdown ->
-      {shutdown, Req, State};
+      {stop, Req, State};
     {shutdown, HandlerState2} ->
-      {shutdown, Req, State#state{handler_state=HandlerState2}}
+      {stop, Req, State#state{handler_state=HandlerState2}}
 end.
 
-websocket_terminate(_Reason, Req, #state{handler=Handler, handler_state=HandlerState}) ->
+terminate(_Reason, Req, #state{handler=Handler, handler_state=HandlerState}) ->
   Handler:handle(terminate, {HandlerState, Req}),
   ok.
